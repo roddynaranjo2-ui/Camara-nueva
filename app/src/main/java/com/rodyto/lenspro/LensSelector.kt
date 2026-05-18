a4fab6c22ac4b4d9fba19d45ee26b8ce747c6cea
roddynaranjo2-ui <roddynaranjo2@gmail.com>
2026-05-18 14:43:20 -0400

    Actualizar LensSelector.kt

--- app/src/main/java/com/rodyto/lenspro/LensSelector.kt
+++ app/src/main/java/com/rodyto/lenspro/LensSelector.kt
@@ -7,6 +7,7 @@ import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -154,7 +155,7 @@ private fun LensBubblePro(
             .then(
                 if (onLongPress != null) {
                     Modifier.pointerInput(Unit) {
-                        androidx.compose.foundation.gestures.detectTapGestures(
+                        detectTapGestures(
                             onLongPress = { onLongPress() }
                         )
                     }
